86f90030a076e51e06cbc3996379d56bb65ea938
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2022-12-03 10:58:05 +0000

    Add files via upload

---
+++ Day1/CountingCalories.py
@@ -0,0 +1,26 @@
+with open('CountingCalories.in') as file:
+    data = [i for i in file.read().strip().split("\n")]
+
+max = 0
+max2 = 0    
+max3 = 0    
+count = 0
+for item in data:
+    if item == '':
+        count = 0       
+    else:
+        num = int(item) )
+        count += num    
+
+    if count > max: 
+        max3 = max2
+        max2 = max
+        max = count
+    elif count > max2:
+        max3 = max2
+        max2 = count     
+    elif count > max3:
+        max3 = count    
+
+print("Part 1 answer: ", max)
+print("Part 2 answer: ", max+max2+max3)
