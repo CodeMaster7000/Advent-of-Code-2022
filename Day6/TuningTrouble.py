139445a40798f7302ca4489e49ca2d10c9207add
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2022-12-07 09:12:59 +0000

    Update TuningTrouble.py

--- Day6/TuningTrouble.py
+++ Day6/TuningTrouble.py
@@ -1,16 +1,12 @@
-import sys
-from copy import deepcopy
-
-infile = sys.argv[1] if len(sys.argv)>1 else 'TuningTrouble.in'
-data = open(infile).read()
-lines = [x for x in data.split('\n')]
-p1 = False
-p2 = False
-
-for i in range(len(data)):
-    if (not p1) and i-3>=0 and len(set([data[i-j] for j in range(4)]))==4:
-        print("Part 1 solution:",i+1)
-        p1 = True
-    if (not p2) and i-13>=0 and len(set([data[i-j] for j in range(14)]))==14:
-        print("Part 2 solution:",i+1)
-        p2 = True
+with open('TuningTrouble.in') as file: 
+     input = file.read() 
+ for i in range(4, len(input)): 
+     s = set(input[(i-4):i]) 
+     if len(s) == 4: 
+         print("Part 1 solution: ", i) 
+         break 
+ for i in range(14, len(input)): 
+     s = set(input[(i-14):i]) 
+     if len(s) == 14: 
+         print("Part 2 solution: ", i) 
+         break
