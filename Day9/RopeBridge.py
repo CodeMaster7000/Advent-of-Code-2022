bc97df151232269ec6e7855a2f96ebf45ccf7728
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2022-12-09 22:22:47 +0000

    Update RopeBridge.py

--- Day9/RopeBridge.py
+++ Day9/RopeBridge.py
@@ -1,33 +1,28 @@
-import sys
-from collections import defaultdict
-infile = sys.argv[1] if len(sys.argv)>1 else 'RopeBridge.in'
-data = open(infile).read().strip()
-lines = [x for x in data.split('\n')]
+from sys import argv
 def adjust(H,T):
     dr = (H[0]-T[0])
     dc = (H[1]-T[1])
-    elif abs(dr)>=2 and abs(dc)>=2:
+    if abs(dr)>=2 and abs(dc)>=2:
         T = (H[0]-1 if T[0]<H[0] else H[0]+1, H[1]-1 if T[1]<H[1] else H[1]+1)
     elif abs(dr)>=2:
         T = (H[0]-1 if T[0]<H[0] else H[0]+1, H[1])
     elif abs(dc)>=2:
         T = (H[0], H[1]-1 if T[1]<H[1] else H[1]+1)
     return T
-H = (0,0)
-T = [(0,0) for _ in range(9)]
-DR = {'L': 0, 'U': -1, 'R': 0, 'D': 1}
-DC = {'L': -1, 'U': 0, 'R': 1, 'D': 0}
-P1 = set([T[0]])
-P2 = set([T[8]])
+lines = open(argv[1] if len(argv)>1 else "Rope Bridge.txt").read().strip().split("\n")
+H = (0, 0)
+T = [(0, 0) for _ in range(9)]
+dirs = {"U":(-1, 0), "R":(0, 1), "D":(1, 0), "L":(0, -1)}
+posT = {(0, 0)}
+pos9 = {(0, 0)}
 for line in lines:
-    d,amt = line.split()
-    amt = int(amt)
-    for _ in range(amt):
-        H = (H[0] + DR[d], H[1]+DC[d])
+    Dir, Len = line.split()
+    for _ in range(int(Len)):
+        H = (H[0] + dirs[Dir][0], H[1] + dirs[Dir][1])
         T[0] = adjust(H, T[0])
         for i in range(1, 9):
             T[i] = adjust(T[i-1], T[i])
-        P1.add(T[0])
-        P2.add(T[8])
-print("Part 1 solution:",(len(P1)))
-print("Part 2 solution:",(len(P2)))
+        posT.add(T[0])
+        pos9.add(T[8])
+print("Part 1 solution:", len(posT))
+print("Part 2 solution:", len(pos9))   
