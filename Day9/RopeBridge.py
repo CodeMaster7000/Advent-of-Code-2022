693c998ccaeed9b20820fef8edcb541c640b67f9
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2022-12-09 18:46:13 +0000

    Create RopeBridge.py

---
+++ Day9/RopeBridge.py
@@ -0,0 +1,35 @@
+import sys
+from collections import defaultdict
+infile = sys.argv[1] if len(sys.argv)>1 else 'RopeBridge.in'
+data = open(infile).read().strip()
+lines = [x for x in data.split('\n')]
+def adjust(H,T):
+    dr = (H[0]-T[0])
+    dc = (H[1]-T[1])
+    if abs(dr)<=1 and abs(dc)<=1:
+        pass
+    elif abs(dr)>=2 and abs(dc)>=2:
+        T = (H[0]-1 if T[0]<H[0] else H[0]+1, H[1]-1 if T[1]<H[1] else H[1]+1)
+    elif abs(dr)>=2:
+        T = (H[0]-1 if T[0]<H[0] else H[0]+1, H[1])
+    elif abs(dc)>=2:
+        T = (H[0], H[1]-1 if T[1]<H[1] else H[1]+1)
+    return T
+H = (0,0)
+T = [(0,0) for _ in range(9)]
+DR = {'L': 0, 'U': -1, 'R': 0, 'D': 1}
+DC = {'L': -1, 'U': 0, 'R': 1, 'D': 0}
+P1 = set([T[0]])
+P2 = set([T[8]])
+for line in lines:
+    d,amt = line.split()
+    amt = int(amt)
+    for _ in range(amt):
+        H = (H[0] + DR[d], H[1]+DC[d])
+        T[0] = adjust(H, T[0])
+        for i in range(1, 9):
+            T[i] = adjust(T[i-1], T[i])
+        P1.add(T[0])
+        P2.add(T[8])
+print("Part 1 solution:",(len(P1)))
+print("Part 2 solution:",(len(P2)))
