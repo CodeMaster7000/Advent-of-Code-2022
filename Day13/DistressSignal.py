21e7610c6f514930bc9f8368f28d427efbdbb7b2
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2022-12-13 18:36:06 +0000

    Update DistressSignal.py

--- Day13/DistressSignal.py
+++ Day13/DistressSignal.py
@@ -1,54 +1,63 @@
-import sys
-import math
-from copy import deepcopy
-from collections import defaultdict, deque
-infile = sys.argv[1] if len(sys.argv)>1 else 'DistressSignal.in'
-data = open(infile).read().strip()
-lines = [x for x in data.split('\n')]
-def compare(p1,p2):
-    if isinstance(p1, int) and isinstance(p2,int):
-        if p1 < p2:
-            return -1
-        elif p1 == p2:
-            return 0
-        else:
-            return 1
-    elif isinstance(p1, list) and isinstance(p2, list):
-        i = 0
-        while i<len(p1) and i<len(p2):
-            c = compare(p1[i], p2[i])
-            if c==-1:
-                return -1
-            if c==1:
-                return 1
-            i += 1
-        if i==len(p1) and i<len(p2):
-            return -1
-        elif i==len(p2) and i<len(p1):
-            return 1
-        else:
-            return 0
-    elif isinstance(p1, int) and isinstance(p2, list):
-        return compare([p1], p2)
-    else:
-        return compare(p1, [p2])
-packets = []
-part1 = 0
-for i,group in enumerate(data.split('\n\n')):
-    p1,p2 = group.split('\n')
-    p1 = eval(p1)
-    p2 = eval(p2)
-    packets.append(p1)
-    packets.append(p2)
-    if compare(p1, p2)==-1:
-        part1 += 1+i
-print("Part 1 solution:",part1)
-packets.append([[2]])
-packets.append([[6]])
-from functools import cmp_to_key
-packets = sorted(packets, key=cmp_to_key(lambda p1,p2: compare(p1,p2)))
-part2 = 1
-for i,p in enumerate(packets):
-    if p==[[2]] or p==[[6]]:
-        part2 *= i+1
-print("Part 2 solution:",part2)
+from __future__ import annotations
+from pathlib import Path
+from ast import literal_eval
+SCRIPT_DIR = Path(__file__).parent
+INPUT_FILE = Path(SCRIPT_DIR, "DistressSignal.in")
+class Packet():   
+    def __init__(self, value) -> None:
+        self.value = value        
+    def __lt__(self, other: Packet) -> bool:
+        if isinstance(self.value, int) and isinstance(other.value, int):
+            if self.value < other.value:
+                return True 
+            if other.value < self.value:
+                return False
+        if isinstance(self.value, int) and isinstance(other.value, list):
+            new_item = Packet([self.value]) 
+            return new_item < other
+        if isinstance(self.value, list) and isinstance(other.value, int):
+            new_item = Packet([other.value]) 
+            return self < new_item
+        if isinstance(self.value, list) and isinstance(other.value, list):
+            compare_count = 0
+            for val in zip(self.value, other.value): 
+                compare_count += 1
+                if val[0] == val[1]:
+                    continue
+                return Packet(val[0]) < Packet(val[1])
+            return len(self.value) < len(other.value)       
+    def __repr__(self) -> str:
+        return str(self.value)    
+class Pair():
+    def __init__(self, left: Packet, right: Packet) -> None:
+        self.left = left
+        self.right = right
+    def __repr__(self):
+        return f"Pair(l={self.left}, r={self.right})"
+def main():
+    with open(INPUT_FILE, mode="rt") as f:
+        data = f.read()      
+    pairs = get_pairs(data)
+    right_order = []    
+    for i, pair in enumerate(pairs, start=1):
+        if pair.left < pair.right:
+            right_order.append(i)      
+    print(f"Part 1 solution: {sum(right_order)}")
+    all_packets = get_all_packets(data)
+    div_two, div_six = Packet([[2]]), Packet([[6]])
+    all_packets.append(div_two)
+    all_packets.append(div_six)
+    sorted_items = sorted(all_packets)
+    loc_div_two = sorted_items.index(div_two) + 1 
+    loc_div_six = sorted_items.index(div_six) + 1
+    print(f"Part 2 solution: {loc_div_two*loc_div_six}")
+def get_pairs(data: str) -> list[Pair]:
+    pairs: list[Pair] = []
+    blocks = data.split("\n\n")  
+    for block in blocks:
+        lines = block.splitlines()
+        pairs.append(Pair(Packet(literal_eval(lines[0])), Packet(literal_eval(lines[1]))))       
+    return pairs
+def get_all_packets(data: str) -> list[Packet]:
+    lines = data.splitlines()
+    return [Packet(literal_eval(line)) for line in lines if line]
